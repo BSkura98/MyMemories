f3b2561a3244f2e5f24492939aa1516dab6e5ece
215877 <q89nTi10>
2020-11-12 22:29:45 +0100

    Deleted some files

--- Server/src/main/java/com/bartlomiejskura/mymemories/MymemoriesApplication.java
+++ Server/src/main/java/com/bartlomiejskura/mymemories/MymemoriesApplication.java
@@ -1,26 +1,12 @@
 package com.bartlomiejskura.mymemories;
 
-import com.bartlomiejskura.mymemories.model.Memory;
-import com.bartlomiejskura.mymemories.repository.MemoryRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MymemoriesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MymemoriesApplication.class, args);
-	}
-
-	@Bean
-	ApplicationRunner applicationRunner(MemoryRepository memoryRepository){
-		return args -> {
-			memoryRepository.save(new Memory("First memory"));
-			memoryRepository.save(new Memory("Second memory"));
-		};
 	}
 }
--- Server/src/main/java/com/bartlomiejskura/mymemories/repository/MemoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bartlomiejskura.mymemories.repository;
-
-import com.bartlomiejskura.mymemories.model.Memory;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemoryRepository extends CrudRepository<Memory, Long> {
-}
