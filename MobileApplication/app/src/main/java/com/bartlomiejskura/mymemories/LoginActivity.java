57d973ca416325e2294d8f8f7a8e35c8da0212bc
215877 <q89nTi10>
2020-11-28 19:28:54 +0100

    [Mobile App] Login and register layouts created

---
+++ MobileApplication/app/src/main/java/com/bartlomiejskura/mymemories/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.bartlomiejskura.mymemories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView signUpTextView = findViewById(R.id.signUpTextView);
+
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+    }
+}
