7e454db9f131bb4ad32fca08ff64db145b5c778c
215877 <q89nTi10>
2020-12-27 22:15:49 +0100

    [Mobile App] Implemented navigation drawer logic

--- MobileApplication/app/src/main/java/com/bartlomiejskura/mymemories/MainActivity.java
+++ MobileApplication/app/src/main/java/com/bartlomiejskura/mymemories/MainActivity.java
@@ -4,19 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.bartlomiejskura.mymemories.fragment.CategoriesFragment;
+import com.bartlomiejskura.mymemories.fragment.MemoryBoardFragment;
+import com.bartlomiejskura.mymemories.fragment.RecentEntriesFragment;
+import com.bartlomiejskura.mymemories.fragment.SettingsFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private NavigationView navigationView;
     private TextView toolbarTextView;
     private DrawerLayout drawerLayout;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toolbarTextView = findViewById(R.id.toolbarTextView);
         Toolbar toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
+        sharedPreferences = getSharedPreferences("MyMemoriesPref", Context.MODE_PRIVATE);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -38,8 +47,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         navigationView.setNavigationItemSelectedListener(this);
 
-        navigationView.setCheckedItem(R.id.nav_recent_entries);
-        toolbarTextView.setText("Recent entries");
+        setFragment();
     }
 
     @Override
@@ -54,6 +62,100 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        Intent i = getIntent();
+        switch (item.getItemId()){
+            case R.id.nav_recent_entries:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RecentEntriesFragment()).commit();
+                toolbarTextView.setText("Recent entries");
+                i.putExtra("fragmentToLoad", "recentEntriesFragment");
+                break;
+            case R.id.nav_memory_board:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MemoryBoardFragment()).commit();
+                toolbarTextView.setText("Memory board");
+                i.putExtra("fragmentToLoad", "memoryBoardFragment");
+                break;
+            case R.id.nav_Categories:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CategoriesFragment()).commit();
+                toolbarTextView.setText("Categories");
+                i.putExtra("fragmentToLoad", "categoriesFragment");
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                toolbarTextView.setText("Settings");
+                i.putExtra("fragmentToLoad", "settingsFragment");
+                break;
+            case R.id.nav_exit:
+                exit();
+                break;
+            case R.id.nav_logout:
+                logOut();
+                return false;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setFragment(){
+        Intent i = getIntent();
+        Bundle bundle = i.getExtras();
+        if(bundle!=null){
+            switch(bundle.getString("fragmentToLoad")){
+                case "memoryBoardFragment":
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MemoryBoardFragment()).commit();
+                    navigationView.setCheckedItem(R.id.nav_memory_board);
+                    toolbarTextView.setText("Memory board");
+                    i.putExtra("fragmentToLoad", "memoryBoardFragment");
+                    break;
+                case "categoriesFragment":
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CategoriesFragment()).commit();
+                    navigationView.setCheckedItem(R.id.nav_Categories);
+                    toolbarTextView.setText("Categories");
+                    i.putExtra("fragmentToLoad", "categoriesFragment");
+                    break;
+                case "settingsFragment":
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                    navigationView.setCheckedItem(R.id.nav_settings);
+                    toolbarTextView.setText("Settings");
+                    i.putExtra("fragmentToLoad", "settingsFragment");
+                    break;
+                case "recentEntriesFragment":
+                default:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RecentEntriesFragment()).commit();
+                    navigationView.setCheckedItem(R.id.nav_recent_entries);
+                    toolbarTextView.setText("Recent entries");
+                    i.putExtra("fragmentToLoad", "recentEntriesFragment");
+                    break;
+            }
+        }else{
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RecentEntriesFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_recent_entries);
+            toolbarTextView.setText("Recent entries");
+            i.putExtra("fragmentToLoad", "recentEntriesFragment");
+        }
+    }
+
+    public void logOut() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("userId");
+        editor.remove("email");
+        editor.remove("firstName");
+        editor.remove("lastName");
+        editor.remove("birthday");
+        editor.apply();
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(i);
+    }
+
+    public void exit(){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
