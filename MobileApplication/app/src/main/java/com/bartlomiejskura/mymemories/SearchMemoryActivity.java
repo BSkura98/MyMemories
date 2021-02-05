4d41c366490a1d80305d6e8d74b2131e1ddd5ea3
215877 <q89nTi10>
2021-02-05 20:03:46 +0100

    [Mobile app] Added recent searches

--- MobileApplication/app/src/main/java/com/bartlomiejskura/mymemories/SearchMemoryActivity.java
+++ MobileApplication/app/src/main/java/com/bartlomiejskura/mymemories/SearchMemoryActivity.java
@@ -2,15 +2,36 @@ package com.bartlomiejskura.mymemories;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.bartlomiejskura.mymemories.adapter.RecentSearchesAdapter;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class SearchMemoryActivity extends AppCompatActivity {
+    private List<String> recentSearches;
+    private SharedPreferences sharedPreferences;
+    private Gson gson = new Gson();
+    private RecentSearchesAdapter adapter;
+    private RecyclerView recentSearchesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +39,7 @@ public class SearchMemoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_memory);
 
         Toolbar toolbar = findViewById(R.id.toolbar_search);
+        recentSearchesList = findViewById(R.id.recentSearchesList);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -27,26 +49,121 @@ public class SearchMemoryActivity extends AppCompatActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                Intent i = new Intent(getApplicationContext(), SearchResultsActivity.class);
-                i.putExtra("query", query);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(i);
+                startSearchResultsActivity(query);
                 return true;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
                 return false;
+            }
+        });
+
+        ImageButton backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(i);
             }
         });
 
         int id = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
         TextView textView = (TextView) searchView.findViewById(id);
         textView.setTextColor(Color.WHITE);
+
+        sharedPreferences = getSharedPreferences("MyMemoriesPref", Context.MODE_PRIVATE);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    getRecentSearches();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(adapter!=null){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    adapter.refresh();
+                }
+            });
+        }
+    }
+
+    public void startSearchResultsActivity(final String query){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                addRecentSearch(query);
+            }
+        }).start();
+
+        Intent i = new Intent(getApplicationContext(), SearchResultsActivity.class);
+        i.putExtra("query", query);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+    }
+
+    private void getRecentSearches() throws JSONException {
+        String json = sharedPreferences.getString("recentSearches", null);
+        if(json == null){
+            recentSearches = new LinkedList<>();
+            return;
+        }
+        JSONArray array = new JSONArray(json);
+        String[] recentSearchesArray = new String[array.length()];
+        for (int i = 0; i < array.length(); i++) {
+            recentSearchesArray[i] = array.getString(i);
+            //JSONObject object = array.getJSONObject(i);
+            //recentSearchesArray[i] = gson.fromJson(object.toString(), String.class);
+        }
+        recentSearches = new LinkedList<>(Arrays.asList(recentSearchesArray));
+
+        final SearchMemoryActivity activity = this;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                adapter = new RecentSearchesAdapter(
+                        getApplicationContext(),
+                        recentSearches,
+                        activity
+                );
+                recentSearchesList.setAdapter(adapter);
+                recentSearchesList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+            }
+        });
+    }
+
+    private void saveRecentSearches(){
+        String json = gson.toJson(recentSearches);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("recentSearches", json);
+        editor.apply();
+    }
+
+    private void addRecentSearch(String search){
+        recentSearches.remove(search);
+
+        recentSearches.add(0, search);
+        if(recentSearches.size()>200){
+            recentSearches.remove(recentSearches.size()-1);
+        }
+        saveRecentSearches();
     }
 }
---
+++ MobileApplication/app/src/main/java/com/bartlomiejskura/mymemories/adapter/RecentSearchesAdapter.java
@@ -0,0 +1,111 @@
+package com.bartlomiejskura.mymemories.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bartlomiejskura.mymemories.R;
+import com.bartlomiejskura.mymemories.SearchMemoryActivity;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RecentSearchesAdapter extends RecyclerView.Adapter<RecentSearchesAdapter.MyViewHolder> implements Filterable {
+    private Context context;
+    private List<String> recentSearches;
+    private List<String> recentSearchesTemp;
+    private SearchMemoryActivity activity;
+
+    public RecentSearchesAdapter(Context context, List<String> recentSearches, SearchMemoryActivity activity) {
+        this.context = context;
+        this.recentSearches = new ArrayList<>(recentSearches);
+        this.activity = activity;
+        recentSearchesTemp = recentSearches;
+    }
+
+    @NonNull
+    @Override
+    public RecentSearchesAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.row_recent_search, parent,false);
+        return new RecentSearchesAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecentSearchesAdapter.MyViewHolder holder, int position) {
+        holder.search.setText(recentSearches.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return Math.min(recentSearches.size(), 20);
+    }
+
+    public void refresh(){
+        notifyItemRangeChanged(0, recentSearches.size());
+    }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    private Filter filter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<String> filteredList = new LinkedList<>();
+
+            if(constraint == null || constraint.length() == 0){
+                filteredList.addAll(recentSearchesTemp);
+            }else{
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for(String search: recentSearchesTemp){
+                    if(search.toLowerCase().contains(filterPattern)){
+                        filteredList.add(search);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            recentSearches.clear();
+            recentSearches.addAll((List)results.values);
+            notifyDataSetChanged();
+        }
+    };
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView search;
+        LinearLayout recentSearchesLinearLayout;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            recentSearchesLinearLayout = itemView.findViewById(R.id.searchLinearLayout);
+            search = itemView.findViewById(R.id.search);
+
+            recentSearchesLinearLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    activity.startSearchResultsActivity(search.getText().toString());
+                }
+            });
+        }
+    }
+}
