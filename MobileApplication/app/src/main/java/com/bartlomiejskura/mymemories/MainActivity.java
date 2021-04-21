7a2289ab890c6a6070b8a2b91108c97e4c529409
215877 <bartek33s@wp.pl>
2021-04-21 12:36:11 +0200

    [Mobile app] Improved navigation drawer

--- MobileApplication/app/src/main/java/com/bartlomiejskura/mymemories/MainActivity.java
+++ MobileApplication/app/src/main/java/com/bartlomiejskura/mymemories/MainActivity.java
@@ -23,11 +23,12 @@ import com.bartlomiejskura.mymemories.fragment.FriendsFragment;
 import com.bartlomiejskura.mymemories.fragment.SettingsFragment;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private TextView toolbarTextView;
     private DrawerLayout drawerLayout;
     private ImageButton searchButton;
+    private Toolbar toolbar;
 
     private SharedPreferences sharedPreferences;
 
@@ -37,34 +38,51 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViews();
+        initValues();
+        setListeners();
+        prepareViews();
+
+        setFragment();
+    }
+
+
+    private void findViews(){
         navigationView = findViewById(R.id.nav_view);
         toolbarTextView = findViewById(R.id.toolbarTextView);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
-        sharedPreferences = getSharedPreferences("MyMemoriesPref", Context.MODE_PRIVATE);
         searchButton = findViewById(R.id.searchButton);
+    }
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), SearchMemoryActivity.class);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(i);
-            }
+    private void initValues(){
+        sharedPreferences = getSharedPreferences("MyMemoriesPref", Context.MODE_PRIVATE);
+    }
+
+    private void setListeners(){
+        searchButton.setOnClickListener(v -> {
+            Intent i = new Intent(getApplicationContext(), SearchMemoryActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
         });
 
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        navigationView.setNavigationItemSelectedListener(new MyOnNavigationItemSelectedListener());
+    }
 
+    private void prepareViews(){
+        //toolbar
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        //drawer layout and navigation view
         navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
-        navigationView.setNavigationItemSelectedListener(this);
-
-        setFragment();
     }
+
 
     @Override
     public void onBackPressed() {
@@ -76,45 +94,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         finish();
     }
 
-    @SuppressLint("SetTextI18n")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Intent i = getIntent();
-        switch (item.getItemId()){
-            case R.id.nav_dates:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MemoriesFragment()).commit();
-                toolbarTextView.setText("Memories");
-                i.putExtra("fragmentToLoad", "datesFragment");
-                searchButton.setVisibility(View.VISIBLE);
-                break;
-            case R.id.nav_friends:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FriendsFragment()).commit();
-                toolbarTextView.setText("Friends");
-                i.putExtra("fragmentToLoad", "friendsMemoriesFragment");
-                searchButton.setVisibility(View.GONE);
-                break;
-            case R.id.nav_Categories:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CategoriesFragment()).commit();
-                toolbarTextView.setText("Categories");
-                i.putExtra("fragmentToLoad", "categoriesFragment");
-                searchButton.setVisibility(View.GONE);
-                break;
-            case R.id.nav_settings:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
-                toolbarTextView.setText("Settings");
-                i.putExtra("fragmentToLoad", "settingsFragment");
-                searchButton.setVisibility(View.GONE);
-                break;
-            case R.id.nav_exit:
-                exit();
-                break;
-            case R.id.nav_logout:
-                logOut();
-                return false;
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
 
     @SuppressLint("SetTextI18n")
     private void setFragment(){
@@ -131,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     break;
                 case "categoriesFragment":
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CategoriesFragment()).commit();
-                    navigationView.setCheckedItem(R.id.nav_Categories);
+                    navigationView.setCheckedItem(R.id.nav_categories);
                     toolbarTextView.setText("Categories");
                     i.putExtra("fragmentToLoad", "categoriesFragment");
                     searchButton.setVisibility(View.GONE);
@@ -146,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 case "datesFragment":
                 default:
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MemoriesFragment()).commit();
-                    navigationView.setCheckedItem(R.id.nav_dates);
+                    navigationView.setCheckedItem(R.id.nav_memories);
                     toolbarTextView.setText("Memories");
                     i.putExtra("fragmentToLoad", "datesFragment");
                     searchButton.setVisibility(View.VISIBLE);
@@ -154,14 +133,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         }else{
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MemoriesFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_dates);
+            navigationView.setCheckedItem(R.id.nav_memories);
             toolbarTextView.setText("Memories");
             i.putExtra("fragmentToLoad", "datesFragment");
             searchButton.setVisibility(View.VISIBLE);
         }
     }
 
-    public void logOut() {
+    public void logout() {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.remove("userId");
         editor.remove("email");
@@ -188,5 +167,49 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         finish();
+    }
+
+
+    private class MyOnNavigationItemSelectedListener implements NavigationView.OnNavigationItemSelectedListener{
+        private MyOnNavigationItemSelectedListener(){}
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Intent i = getIntent();
+            switch (item.getItemId()){
+                case R.id.nav_memories:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MemoriesFragment()).commit();
+                    toolbarTextView.setText("Memories");
+                    i.putExtra("fragmentToLoad", "datesFragment");
+                    searchButton.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.nav_friends:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FriendsFragment()).commit();
+                    toolbarTextView.setText("Friends");
+                    i.putExtra("fragmentToLoad", "friendsMemoriesFragment");
+                    searchButton.setVisibility(View.GONE);
+                    break;
+                case R.id.nav_categories:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CategoriesFragment()).commit();
+                    toolbarTextView.setText("Categories");
+                    i.putExtra("fragmentToLoad", "categoriesFragment");
+                    searchButton.setVisibility(View.GONE);
+                    break;
+                case R.id.nav_settings:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                    toolbarTextView.setText("Settings");
+                    i.putExtra("fragmentToLoad", "settingsFragment");
+                    searchButton.setVisibility(View.GONE);
+                    break;
+                case R.id.nav_exit:
+                    exit();
+                    break;
+                case R.id.nav_logout:
+                    logout();
+                    return false;
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        }
     }
 }
